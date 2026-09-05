98a0e2226dbadc489bebc2c2f98e7d2f053bf03d
África-de-Aragón <africa.aragon@luca-tic.com>
2026-09-05 17:03:47 +0200

    Update remote execution command for USS

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -4,6 +4,8 @@ set -e
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 ZOWE_ARGS="--host $ZOWE_HOST --port ${ZOWE_PORT:-10443} --user $ZOWE_USERNAME --pass $ZOWE_PASSWORD --reject-unauthorized false"
 
-echo "Ejecutando permisos y pruebas de COBOL Check remotamente en USS..."
-zowe zos-uss issue command "chmod +x /z/$LOWERCASE_USERNAME/cobolcheck/scripts/*" $ZOWE_ARGS
-zowe zos-uss issue command "cd /z/$LOWERCASE_USERNAME/cobolcheck && ./scripts/linux_gnucobol_run_tests" $ZOWE_ARGS
+echo "Asignando permisos de ejecución en USS..."
+zowe zos-files issue command "chmod -R +x /z/$LOWERCASE_USERNAME/cobolcheck" $ZOWE_ARGS || true
+
+echo "Ejecutando suite de pruebas COBOL Check en USS..."
+zowe zos-uss issue command "cd /z/$LOWERCASE_USERNAME/cobolcheck && ./cobolcheck" $ZOWE_ARGS

f57479cfa4d991877d00573f0e4efffc0cf31fcd
África-de-Aragón <africa.aragon@luca-tic.com>
2026-09-05 17:05:57 +0200

    Fix zos-uss command syntax for remote execution

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -4,8 +4,7 @@ set -e
 LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
 ZOWE_ARGS="--host $ZOWE_HOST --port ${ZOWE_PORT:-10443} --user $ZOWE_USERNAME --pass $ZOWE_PASSWORD --reject-unauthorized false"
 
-echo "Asignando permisos de ejecución en USS..."
-zowe zos-files issue command "chmod -R +x /z/$LOWERCASE_USERNAME/cobolcheck" $ZOWE_ARGS || true
+echo "Asignando permisos de ejecución y corriendo pruebas COBOL Check en USS..."
 
-echo "Ejecutando suite de pruebas COBOL Check en USS..."
-zowe zos-uss issue command "cd /z/$LOWERCASE_USERNAME/cobolcheck && ./cobolcheck" $ZOWE_ARGS
+# Ejecutamos chmod e invocamos las pruebas en una sola llamada de comando a zos-uss
+zowe zos-uss issue command "chmod -R +x /z/$LOWERCASE_USERNAME/cobolcheck && cd /z/$LOWERCASE_USERNAME/cobolcheck && ./cobolcheck" $ZOWE_ARGS
